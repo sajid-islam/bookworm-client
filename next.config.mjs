4edd9e61ec5821358336e561d9d3d0a5d2f0f60f
sajid-islam <sajidislam941@gmail.com>
2026-01-15 01:02:40 +0600

    fix: rewrites api

--- next.config.mjs
+++ next.config.mjs
@@ -14,7 +14,7 @@ const nextConfig = {
         return [
             {
                 source: "/api/:path*",
-                destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/:path*`,
+                destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/:path*`,
             },
         ];
     },
