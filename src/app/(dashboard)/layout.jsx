9d1c0d1b392df309c3875e14c6d54cf31313b72e
sajid-islam <sajidislam941@gmail.com>
2026-01-15 02:34:08 +0600

    fix build error

--- src/app/(dashboard)/layout.jsx
+++ src/app/(dashboard)/layout.jsx
@@ -1,14 +1,12 @@
 import { AppSidebar } from "@/components/app-sidebar";
-import ProtectRoute from "@/components/ProtectedRoute/ProtectedRoute";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 
 const DashboardLayout = ({ children }) => {
     return (
-            <SidebarProvider>
-                {" "}
-                <AppSidebar />
-                <SidebarInset>{children}</SidebarInset>
-            </SidebarProvider>{" "}
+        <SidebarProvider>
+            <AppSidebar />
+            <SidebarInset>{children}</SidebarInset>
+        </SidebarProvider>
     );
 };
 
